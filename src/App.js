c18ae7953a684db614ed4723983a5c2263cd9d73
Nicholas Carter <nicholas.b.carter@gmail.com>
2016-12-28 23:09:28 -0500

    more

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
-import { graphql } from 'react-apollo';
+import { ApolloProvider } from "react-apollo";
 
 import Pokemon from "./Pokemon";
 

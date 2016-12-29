e38026377cdee91a8a9f22d9e01a27b84b13fff6
Nicholas Carter <nicholas.b.carter@gmail.com>
2016-12-28 23:01:43 -0500

    more to come

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { graphql } from 'graphql-tag';
+
+import Pokemon from "./Pokemon";
+
+export const networkInterface = createNetworkInterface({ uri: 'https://graphql-pokemon.now.sh/' });
+export const client = new ApolloClient({ networkInterface });
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <Pokemon />
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
